ced26dc50081501eaf9cde1a5ce56e9d72a8f612
Shouvik Guha <59603436+shouvikguha@users.noreply.github.com>
2024-02-05 04:12:59 -0800

    compare.sh

--- compare.sh
+++ compare.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
-#
-# Name: Shouvik Guha
-# PID: A15990598
-# Account ID: cs15lfa20aoz
-# File: compare.sh
-# Assignment: Scripting Project 1
-# Date: 18/11/20
-#
-#
-#===============================================================================
-# DO NOT TOUCH BELOW THIS LINE
-#===============================================================================
+
 
 ERR_USAGE="Usage: ./compare.sh myprog solprog
        ./compare.sh -i test_input myprog solprog"
@@ -19,11 +8,6 @@ MY_OUTPUT="tmp_prog.txt"
 SOL_OUTPUT="tmp_sol.txt"
 DIFF_RESULTS="results.txt"
 
-#===============================================================================
-# DO NOT TOUCH ABOVE THIS LINE
-#
-#
-# ========================== YOUR CODE STARTS BELOW ===========================
 
 # check_results
 # Function that compares two input files using diff. If the files are different,
@@ -42,11 +26,10 @@ function check_results {
     output1="$1"
     output2="$2"
 
-    # TODO: Compare the results using diff and save it to the filename stored in
-    # the variable DIFF_RESULTS
+   
     diff "${output1}" "${output2}" > "${DIFF_RESULTS}"
 
-    # TODO: Check if the file stored in DIFF_RESULTS contains any characters
+  
     if [ -s ${DIFF_RESULTS} ];then
         echo "Outputs are not identical. :("
         echo "Check out ${DIFF_RESULTS} for more details" >&2
@@ -63,8 +46,6 @@ function check_results {
 # USAGE
 #   clean
 function clean {
-    # TODO: Remove the temporary output files stored in the variables
-    # MY_OUTPUT and SOL_OUTPUT
     rm -f ${MY_OUTPUT} ${SOL_OUTPUT}
 }
 
@@ -78,14 +59,11 @@ function clean {
 #   myprog: name of script or program that will be tested against the solution
 #   solprog: name of the solution implementation of the program
 function prog_compare {
-    # TODO: Set incoming arguments to correct variables
+ 
     program="$1"
     solution="$2"
 
-    # TODO: Run both the program and the solution and save the output to the
-    # correct temporary output file (hint: myprog should save to the filename
-    # stored in MY_OUTPUT and solprog should save to the filename stored in
-    # SOL_OUTPUT)
+    
     "${program}" > "${MY_OUTPUT}"
     "${solution}" > "${SOL_OUTPUT}"
 
@@ -106,16 +84,14 @@ function prog_compare {
 #   myprog: name of script or program that will be tested against the solution
 #   solprog: name of the solution implementation of the program
 function prog_compare_with_input {
-    # TODO: Set incoming arguments to correct variables
+   
     program="$2"
     solution="$3"
     input_file="$1"
 
-    # TODO: Check if the input_file exists
+    
     if [ -f "${input_file}" ]; then
-        # TODO: If it does exist, run both the program and the solution using
-        # input_file as the input to the two programs and outputting to the
-        # correct temporary output file
+       
         "${program}" < "${input_file}" > "${MY_OUTPUT}"
         "${solution}" < "${input_file}" > "${SOL_OUTPUT}"
     else
@@ -137,16 +113,14 @@ function prog_compare_with_input {
 # INPUT
 #   prog: name of a script or program
 function check_permissions {
-    # TODO: Set incoming arguments to correct variables
+    
     program="$1"
 
-    # TODO: Check if the file exists and if it can be read and executed
     if [ -e "${program}"  -a  -r "${program}"  -a  -x "${program}" ]; then
         echo "${program} is valid"
     else
         echo "Error: Bad permissions or file does not exist (${program})" >&2
 
-        # TODO: Terminate the script with exit status code 1
         exit 1
     fi
 }
@@ -160,13 +134,9 @@ function check_permissions {
 function print_error_and_exit {
     echo "${ERR_USAGE}" >&2
 
-    # TODO: Terminate the script with exit status code 1
     exit 1
 }
 
-#===============================================================================
-# DO NOT TOUCH BELOW THIS LINE
-#===============================================================================
 
 function ??? {
     echo "WARNING! TODO on line ${BASH_LINENO[0]} not implemented (or ??? was" \
@@ -197,7 +167,3 @@ function main {
 }
 
 main "$@"
-
-#===============================================================================
-# DO NOT TOUCH ABOVE THIS LINE
-#===============================================================================
