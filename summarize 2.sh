8c898bff40289ac82bcd029e9ccd6f2fc37b7aef
Shouvik Guha <59603436+shouvikguha@users.noreply.github.com>
2024-02-05 04:17:43 -0800

    summarize 2.sh

--- summarize 2.sh
+++ summarize 2.sh
@@ -1,18 +1,4 @@
 #!/bin/bash
-# TODO: Complete the first line of the script with the correct location for
-# bash
-#
-# Name: Shouvik Guha
-# PID: A15990598
-# Account ID: cs15lfa20aoz
-# File: summarize.sh
-# Assignment: Scripting Project 3
-# Date: 08/12/20
-#
-#
-#===============================================================================
-# DO NOT TOUCH BELOW THIS LINE
-#===============================================================================
 
 LIST_TITLES=("Moby-Dick; or, The Whale" \
              "Gadsby"                   \
@@ -81,9 +67,7 @@ function download_book {
 
 OFFSET_A=`atoi A`
 
-#===============================================================================
-# DO NOT TOUCH ABOVE THIS LINE
-# ========================== YOUR CODE STARTS BELOW ===========================
+
 
 NUM_LINES_TO_PROCESS=100    # Controls the number of lines that are processed
 
@@ -99,8 +83,6 @@ NUM_LINES_TO_PROCESS=100    # Controls the number of lines that are processed
 # USAGE
 #   check_library
 function check_library {
-    # TODO: Check if the directory exists and implement the functionality as
-    # described in the function description.
     if [[ -d "${DIR_LIBRARY}" ]]; then
 	LIBPATH=$(pwd)/"${DIR_LIBRARY}" # Changed hardcoded path to dynamic one
         echo "Found library at ${LIBPATH}"
@@ -124,13 +106,8 @@ function check_library {
 # INPUT
 #   ind: index of the book to be checked
 function check_book {
-    # TODO: Create a variable that contains the path to the book selected by the
-    # first input argument
-  
     variable="${DIR_LIBRARY}"/"${LIST_FILES["$1"]}"
 
-    # TODO: Check if the file at the defined path exists and implement the
-    # functionality as described in the function description.
     if [[ -e "${variable}" ]]; then 
         return 0
     else
@@ -155,41 +132,30 @@ function count_characters {
 
     print_line_statistics "$path_in"
 
-    # TODO: Initialize array to store the counts of the different ASCII
-    # characters. Since there are 256 possible ASCII characters,
     Array=()
 
-    # TODO: Initialize the array with 256 elements and set each element equal
-    # to 0.
     for (( i=0; i < 256; i++ )); do
         Array+=(0)
     done
 
-    # TODO: Iterate through a file line-by-line from the first line for
-    # NUM_LINES_TO_PROCESS consecutive lines. Count the number of occurrences
-    # of each ASCII character and save it to the above defined array.
+
     line_num=0
     while read line_in; do
         ((line_num++))
         #echo "Line ${line_num}" # This line may be useful for debugging
 
-        # TODO: Iterate through a line character-by-character and add 1 to the
-        # correct element in the array. Use `atoi $character` to get the proper
-        # index where character is the current character.
         for ((inner=0; inner < ${#line_in}; inner++)); do
 	    character=${line_in:$inner:`expr $inner + 1`}
 	    (( Array[`atoi "$character"`]++ ))
         done
 
 
-        # TODO: After processing NUM_LINES_TO_PROCESS lines, exit from the loop
         if [[ "${line_num}" -eq "$NUM_LINES_TO_PROCESS" ]] ; then
             break
         fi
     done < "${path_in}"
 
-    # TODO: Print the counts to the terminal. For a given letter, sum the counts
-    # of the lowercase and uppercase variants of the letter.
+
     echo
     echo "--Letter Count--"
     for letter in "${LETTERS[@]}"; do
@@ -217,11 +183,7 @@ function count_characters {
 function print_booklist {
     echo
     echo "The following books are available:"
-
-    # TODO: Print out the list of books as described in the function description
-    # so that it matches the example. (HINT: You can use check_book, which
-    # checks if the text file of the selected book exists in the library. Which
-    # type of quotes can you use to capture the output of a function?)
+    
     for (( i=0; i < "${#LIST_TITLES[@]}"; i++ )); do
 	    listval=`expr ${i} + 1`
 	    combine="${listval}) "${LIST_TITLES[$i]}" by "${LIST_AUTHORS[$i]}" `check_book $i`"
@@ -243,8 +205,7 @@ function print_booklist {
 #   path: path to the file being processed (example "path/file.txt")
 function print_line_statistics {
     path_in="$1"
-    # TODO: Implement the functionality described in the function description
-    # add your code here and modify the print statements.
+
     numolines=`wc -l < "${path_in}"`
     echo "${path_in} has ${numolines} lines"
     var=$(echo "$NUM_LINES_TO_PROCESS/$numolines*100" | bc -l)
@@ -260,7 +221,6 @@ function print_line_statistics {
 # USAGE
 #   print_input_error
 function print_input_error_and_exit {
-    # TODO: Implement the functionality  described in the function description
     echo "Error: "${ERR_INPUT}"">&1
     exit 1
 }
@@ -281,10 +241,6 @@ function main {
     print_booklist
 
     
-    # TODO: Prompt the user for which book they would like to process and save
-    # the result in the variable ind_book. (HINT: The printed book list starts
-    # at 1 but bash lists are indexed by 0. Solve this however you like! But
-    # make sure that your code is consistent.)
     echo -n "$MSG_PROMPT"
     read ind_book
     ind_book=$(($ind_book - 1))
@@ -297,12 +253,8 @@ function main {
         # can be inverted using !.
 	print_input_error_and_exit
    
-    # TODO: Check that the user input is in a valid range as defined by the
-    # number of elements in LIST_TITLES
+    
     else
-        # TODO: Form the path to the .txt file of the book (this variable will
-        # be used later!). It would be wise to create a variable to save the
-        # path.
         var="${DIR_LIBRARY}"/"${LIST_FILES[$ind_book]}"
 
         # Check if the book has been downloaded already by using the check_book
@@ -314,15 +266,10 @@ function main {
             download_book "${ind_book}"
         fi
 
-        # TODO: Count the frequency of characters by calling the function
-        # count_characters with the newly formed path as the argument.
         count_characters "$var"
     fi
 }
 
-#===============================================================================
-# DO NOT TOUCH BELOW THIS LINE
-#===============================================================================
 
 function ??? {
     echo "WARNING! TODO on line ${BASH_LINENO[0]} not implemented (or ??? was" \
@@ -336,4 +283,4 @@ fi
 
 main
 
-#===============================================================================
+
